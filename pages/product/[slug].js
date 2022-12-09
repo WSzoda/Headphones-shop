91039e672346644cfb2b17d2a5ad18604ebd5efc
Wowosz <wojciechszoda@icloud.com>
2022-12-09 09:39:01 +0100

    Added displeying images and product name

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -11,6 +11,21 @@ const ProductDetails = ({ product, products }) => {
                 <div className='image-container'>
                     <img src={urlFor(image && image[0])} />
                 </div>
+                {/* <div className='small-images-container'>
+                    {image?.map((item, i) => (
+                        <img src={urlFor(item)}
+                        className=''
+                        onMouseEnter=''/>
+                    ))}
+                </div> */}
+            </div>
+            <div className='product-details-desc'>
+                <h1>{name}</h1>
+                <div className='reviews'>
+                    <div>
+                        
+                    </div>
+                </div>
             </div>
         </div>
     </div>
